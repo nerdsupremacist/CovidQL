c802d6e5b2b692d13b27bda590352692f0bad6bb
Mathias Quintero <me@quintero.io>
2020-03-26 13:46:45 +0100

    Trying to set the environment properly

--- Sources/CovidQL/main.swift
+++ Sources/CovidQL/main.swift
@@ -6,11 +6,14 @@ import Vapor
 import GraphZahlVaporSupport
 import Runtime
 
-let app = Application()
+let app = Application(try .detect())
 let base = "https://corona.lmao.ninja"
 
 if let port = ProcessInfo.processInfo.environment["PORT"].flatMap(Int.init) {
     app.server.configuration.port = port
+} else if case .production = app.environment {
+    app.server.configuration.port = 80
+    app.server.configuration.hostname = "0.0.0.0"
 }
 
 app.routes.graphql(use: CovidQL.self, includeGraphiQL: true) { request in
