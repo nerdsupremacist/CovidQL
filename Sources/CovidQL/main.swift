4ffa3b8ef9ee99c605ce644c6ebfcfcd54225ef7
Mathias Quintero <me@quintero.io>
2020-03-26 13:55:01 +0100

    Removing extra stuff

--- Sources/CovidQL/main.swift
+++ Sources/CovidQL/main.swift
@@ -9,13 +9,6 @@ import Runtime
 let app = Application(try .detect())
 let base = "https://corona.lmao.ninja"
 
-if let port = ProcessInfo.processInfo.environment["PORT"].flatMap(Int.init) {
-    app.server.configuration.port = port
-} else if case .production = app.environment {
-    app.server.configuration.port = 80
-    app.server.configuration.hostname = "0.0.0.0"
-}
-
 app.routes.graphql(use: CovidQL.self, includeGraphiQL: true) { request in
     return Client(base: base, httpClient: HTTPClient(eventLoopGroupProvider: .shared(request.eventLoop)))
 }
