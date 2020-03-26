f0d1ef064a9b6a3764b405924ecfb69f7b745490
Mathias Quintero <me@quintero.io>
2020-03-26 11:41:19 +0100

    Adding port setting from environment

--- Sources/CovidQL/main.swift
+++ Sources/CovidQL/main.swift
@@ -9,6 +9,10 @@ import Runtime
 let app = Application()
 let base = "https://corona.lmao.ninja"
 
+if let port = ProcessInfo.processInfo.environment["PORT"].flatMap(Int.init) {
+    app.server.configuration.port = port
+}
+
 app.routes.graphql(use: CovidQL.self, includeGraphiQL: true) { request in
     return Client(base: base, httpClient: HTTPClient(eventLoopGroupProvider: .shared(request.eventLoop)))
 }
