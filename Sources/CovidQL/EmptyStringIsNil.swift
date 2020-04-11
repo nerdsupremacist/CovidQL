4a9d8c778a4807b00e7cd853603e82a08aeb24f7
Mathias Quintero <me@quintero.io>
2020-04-11 21:43:01 +0200

    Fixing crash in case of image url in news

--- Sources/CovidQL/EmptyStringIsNil.swift
+++ Sources/CovidQL/EmptyStringIsNil.swift
@@ -1,6 +1,9 @@
 
 import Foundation
 import GraphZahl
+import GraphQL
+import ContextKit
+import NIO
 
 @propertyWrapper
 struct EmptyStringIsNil<Value: Decodable> : Decodable {
@@ -13,5 +16,25 @@ struct EmptyStringIsNil<Value: Decodable> : Decodable {
         } else {
             wrappedValue = try Optional<Value>(from: decoder)
         }
+    }
+}
+
+extension EmptyStringIsNil: Resolvable where Value: Resolvable { }
+
+extension EmptyStringIsNil: OutputResolvable where Value: OutputResolvable {
+    static var additionalArguments: [String : InputResolvable.Type] {
+        return Value.additionalArguments
+    }
+
+    static func reference(using context: inout Resolution.Context) throws -> GraphQLOutputType {
+        return try context.reference(for: Value?.self)
+    }
+
+    static func resolve(using context: inout Resolution.Context) throws -> GraphQLOutputType {
+        return try context.resolve(type: Value?.self)
+    }
+
+    func resolve(source: Any, arguments: [String : Map], context: MutableContext, eventLoop: EventLoopGroup) throws -> EventLoopFuture<Any?> {
+        return try wrappedValue?.resolve(source: source, arguments: arguments, context: context, eventLoop: eventLoop) ?? eventLoop.future(nil)
     }
 }
