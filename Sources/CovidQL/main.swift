2172076b349f35c73a60399e92a614ab95abef62
Mathias Quintero <me@quintero.io>
2020-08-25 09:32:45 +0200

    let’s see if this helps

--- Sources/CovidQL/main.swift
+++ Sources/CovidQL/main.swift
@@ -16,11 +16,11 @@ let newsBase = "https://newsapi.org/v2/"
 extension Client {
 
     static func client(for request: Request? = nil) -> Client {
-        #if os(Linux)
-        let ipAddress = request?.headers.firstValue(name: HTTPHeaders.Name("X-Forwarded-For")) ?? request?.remoteAddress?.ipAddress
-        #else
+//        #if os(Linux)
+//        let ipAddress = request?.headers.firstValue(name: HTTPHeaders.Name("X-Forwarded-For")) ?? request?.remoteAddress?.ipAddress
+//        #else
         let ipAddress = request?.headers.forwarded.first?.for ?? request?.remoteAddress?.ipAddress
-        #endif
+//        #endif
         return Client(ipAddress: ipAddress,
                       covidBase: covidBase,
                       ipAPIBase: ipAPIBase,
